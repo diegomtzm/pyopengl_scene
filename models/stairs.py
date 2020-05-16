b38abbeb72fbd1d0ae0de38a37bc60071e474129
Diego Martinez <diegomtz1998@hotmail.com>
2020-05-16 18:08:23 -0500

    Add stairs to the final scene

--- models/stairs.py
+++ models/stairs.py
@@ -19,24 +19,32 @@ stairs_f = [(20,21,32,31),(21,22,33,32),(22,23,34,33),(23,24,35,34),(24,25,36,35
 stairsFaces = wall1_f + wall2_f + stairs_f
 
 # Function to draw the stairs
-def stairs():
+def stairs(translate=(0,0,0), rotate=(0,0,0), scale=(1,1,1)):
+    # Push an pop matrix to apply transformations only to this object
+    glPushMatrix()
+    glRotate(-90,1,0,0)
+    glRotate(90,0,0,1)
+    glTranslate(translate[0],translate[1],translate[2])
+    glScale(scale[0], scale[1], scale[2])
+    glColor3fv((.8,.8,.8))
+
     glBegin(GL_POLYGON)
-    glColor3fv((1,0,0))
     for wallsQuad in wall2_f:
         for wallsVertex in wallsQuad:
             glVertex3fv(walls_v[wallsVertex])
     glEnd()
 
     glBegin(GL_POLYGON)
-    glColor3fv((0,0,1))
     for wallsQuad in wall1_f:
         for wallsVertex in wallsQuad:
             glVertex3fv(walls_v[wallsVertex])
     glEnd()
 
-    glBegin(GL_QUADS)   
-    glColor3fv((0,1,0))
+    glBegin(GL_QUADS) 
+    glColor3fv((.72,.72,.72))  
     for stairsQuad in stairs_f:
         for stairsVertex in stairsQuad:
             glVertex3fv(stairsVerts[stairsVertex])
     glEnd()
+
+    glPopMatrix()
--- scene.py
+++ scene.py
@@ -53,19 +53,26 @@ def main():
 
         # draw models into the scene
         environment()
-        house(translate=(2.8, 1, 0),        scale=(0.75,0.75,0.75), base=(0.639, 1, 0.729),     roof=(0.513, 0.8, 0.584))
-        house(translate=(2.8, -2, -0.1),    scale=(0.75,0.75,0.75), base=(1, 0.670, 0.568),     roof=(1, 0.592, 0.470)) 
-        house(translate=(2.8, -5, -0.3),    scale=(0.75,0.75,0.75), base=(0.466, 0.749, 0.670), roof=(0.372, 0.6, 0.537))
-        house(translate=(2.8, -8, -0.5),    scale=(0.75,0.75,0.75), base=(0.901, 0.647, 0.701), roof=(0.749, 0.541, 0.584))
-        tree(translate=(3.2,1.6,0))
-        tree(translate=(0.6,2.4,0.15))
-        tree(translate=(-2.4,3.3,0.3))
+        house(translate=(2.8, 1, 0), scale=(0.75,0.75,0.75), base=(0.639, 1, 0.729), roof=(0.513, 0.8, 0.584))
+        house(translate=(2.8, -2, -0.1), scale=(0.75,0.75,0.75), base=(1, 0.670, 0.568), roof=(1, 0.592, 0.470)) 
+        house(translate=(2.8, -5, -0.3), scale=(0.75,0.75,0.75), base=(0.466, 0.749, 0.670), roof=(0.372, 0.6, 0.537))
+        house(translate=(2.8, -8, -0.5), scale=(0.75,0.75,0.75), base=(0.901, 0.647, 0.701), roof=(0.749, 0.541, 0.584))
+        trashcan(translate=(3.99,1.8,0), scale=(0.75, 1, 1))
+        stairs(translate=(2.8, -3.1, 0), scale=(0.6, 0.6, 0.6))
+        trashcan(translate=(1.2,2,0.15), scale=(0.75, 1, 1))
+        stairs(translate=(3, -0.6, 0), scale=(0.6, 0.6, 0.6))
+        stairs(translate=(3.7, 2.3, 0), scale=(0.55, 0.55, 0.55))
+        trashcan(translate=(-1.4,2.8,0.3), scale=(0.75, 1, 1))
+        stairs(translate=(3.8, 5, 0), scale=(0.6, 0.6, 0.6))
+        tree(translate=(3.2,1.2,0))
+        tree(translate=(0,2.4,0.15))
+        tree(translate=(-2.5,3.3,0.3))
         car(translate=(-5.8,-3.5,0), scale=(0.75,0.75,0.75), color=(0.64, 0, 0.14))
         car(translate=(-1.8,-3.65,0), scale=(0.75,0.75,0.75), color=(0.17,0.32,0.49))
         car(translate=(2.3,-3.65,0), scale=(0.75,0.75,0.75), color=(0.87, 0.58, 0.24))
-        trashcan(translate=(3.99,1.8,0))
-        trashcan(translate=(1.2,2.6,0.15))
-        trashcan(translate=(-1.9,3.5,0.3))
+        
+        
+        
         
         # Updates the window with the active buffer contents
         pg.display.flip()
